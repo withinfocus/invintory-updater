74f4b2d9d867e2dcfb146b48074888c9213772ef
withinfocus <withinfocus@users.noreply.github.com>
2024-02-25 18:11:02 +0000

    Apply lint fixes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 var hostBuilder = Host.CreateApplicationBuilder(args);
