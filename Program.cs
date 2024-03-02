aa9af6a75e3e46ddc0f1e835ebe7ef88d23b0bda
Matt Bishop <matt@withinfocus.com>
2024-03-02 09:23:30 -0500

    Utilize refresh token and exchange for access token at startup

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
+
+const string GoogleApiKey = "***REMOVED***";
+var httpClient = new HttpClient();
 
 var hostBuilder = Host.CreateApplicationBuilder(args);
 hostBuilder.Configuration.AddUserSecrets<Program>();
@@ -11,16 +15,13 @@ using var scope = host.Services.CreateScope();
 
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 
+var token = await RetrieveTokenAsync();
+if (token == null)
+    return -1;
+
 if (args.Length == 0)
 {
     logger.LogError("No operations specified.");
-    return -1;
-}
-
-var token = hostBuilder.Configuration["AuthorizationToken"];
-if (string.IsNullOrEmpty(token) || token == "SECRET")
-{
-    logger.LogError("No authorization token secret provided.");
     return -1;
 }
 
@@ -42,6 +43,41 @@ foreach (var arg in args)
 }
 
 return 0;
+
+async Task<string?> RetrieveTokenAsync()
+{
+    var refreshToken = hostBuilder.Configuration["RefreshToken"];
+    if (string.IsNullOrEmpty(refreshToken) || refreshToken == "SECRET")
+    {
+        logger.LogError("No refresh token secret provided.");
+        return null;
+    }
+
+    try
+    {
+        using HttpResponseMessage response = await httpClient.PostAsync(
+            $"https://securetoken.googleapis.com/v1/token?key={GoogleApiKey}",
+            new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                ["grant_type"] = "refresh_token",
+                ["refresh_token"] = refreshToken
+            }));
+        response.EnsureSuccessStatusCode();
+
+        var tokenResponse = await response.Content.ReadFromJsonAsync<Dictionary<string, string>>();
+        if (tokenResponse == null || tokenResponse["access_token"] == null)
+        {
+            return null;
+        }
+
+        return tokenResponse["access_token"];
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "Failed to authorize.");
+        return null;
+    }
+}
 
 void UpdateCollectionPrices()
 {
